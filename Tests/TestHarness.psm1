622b8dd735e1a78dbd5735c399875eebe0c5052c
Mike Poulson <mikepo@amazon.com>
2026-02-21 20:05:39 -0800

    Focus tests

--- Tests/TestHarness.psm1
+++ Tests/TestHarness.psm1
@@ -55,7 +55,7 @@ function Invoke-TestHarness
     $getChildItemParameters = @{
         Path    = (Join-Path -Path $repoDir -ChildPath '\Tests\Unit')
         Recurse = $true
-        Filter  = '*.Tests.ps1'
+        Filter  = 'Microsoft365DSC.AADPermissionGrantPolicy.Tests.ps1'
     }
 
     # Get all tests '*.Tests.ps1'.
