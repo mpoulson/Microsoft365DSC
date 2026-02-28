ed49ffe1ed35b35d0cf8a44da6bc06288ffe9aca
Mike Poulson <mikepo@amazon.com>
2026-02-28 15:44:54 -0800

    Add examples

---
+++ patch.bat
@@ -0,0 +1,21 @@
+@echo off
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADAppManagementPolicy\*" MSFT_AADAppManagementPolicy\ /y
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADCertificateBasedApplicationConfiguration\*" MSFT_AADCertificateBasedApplicationConfiguration\ /y
+
+rem mkdir MSFT_AADPermissionGrantPolicy
+rem mkdir MSFT_AADPermissionGrantPolicyInclude
+rem mkdir MSFT_AADPermissionGrantPolicyExclude
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADPermissionGrantPolicy\*" MSFT_AADPermissionGrantPolicy\ /y
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADPermissionGrantPolicyInclude\*" MSFT_AADPermissionGrantPolicyInclude\ /y
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADPermissionGrantPolicyExclude\*" MSFT_AADPermissionGrantPolicyExclude\ /y
+
+Rem ***** AADFederationConfiguration
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADFederationConfiguration\*" MSFT_AADFederationConfiguration\ /y
+
+echo **** MSFT_IntuneCorporateDeviceIdentifier
+rem mkdir MSFT_IntuneCorporateDeviceIdentifier
+rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_IntuneCorporateDeviceIdentifier\*" MSFT_IntuneCorporateDeviceIdentifier\ /y
+
+Rem *** MSFT_AADRoleAssignmentScheduleRequest
+Rem copy "C:\src\repos\github\mpoulson-Microsoft365DSC\Modules\Microsoft365DSC\DSCResources\MSFT_AADRoleAssignmentScheduleRequest\*" MSFT_AADRoleAssignmentScheduleRequest\ /y
+
